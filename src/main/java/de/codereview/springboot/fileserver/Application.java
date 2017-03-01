ef80891ed79f216ada9c6a7da97a0f9022f91794
Frank Bruch <frank.bruch@shks.de>
2017-03-01 17:43:35 +0100

    rebranding demo application

--- src/main/java/de/codereview/springboot/fileserver/Application.java
+++ src/main/java/de/codereview/springboot/fileserver/Application.java
@@ -1,12 +1,12 @@
-package com.example;
+package de.codereview.springboot.fileserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
-
+public class Application
+{
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 }
--- src/test/java/de/codereview/springboot/fileserver/ApplicationTests.java
+++ src/test/java/de/codereview/springboot/fileserver/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package de.codereview.springboot.fileserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class DemoApplicationTests {
+public class ApplicationTests
+{
 
 	@Test
 	public void contextLoads() {
