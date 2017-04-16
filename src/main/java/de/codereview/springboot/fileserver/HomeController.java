bc6ddae5d2b0f550ed6f2f186899b1d6689ad53e
Frank Bruch <frank.bruch@shks.de>
2017-04-16 11:44:27 +0200

    Spring Boot Actuator

--- src/main/java/de/codereview/springboot/fileserver/HomeController.java
+++ src/main/java/de/codereview/springboot/fileserver/HomeController.java
@@ -2,20 +2,26 @@ package de.codereview.springboot.fileserver;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.embedded.LocalServerPort;
-import org.springframework.util.MimeType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.endpoint.Endpoint;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/")
 public class HomeController
 {
 	private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    private List<Endpoint> endpoints;
+
+    @Autowired
+    public HomeController(List<Endpoint> endpoints) {
+        this.endpoints = endpoints;
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET, produces = "text/html")
 	public Object index()
@@ -27,8 +33,14 @@ public class HomeController
         builder.append("springboot-fileserver");
         builder.append("</h2><ul>");
         builder.append("<li><a href=\"fb\">fileservice-browser</a></li>");
+        builder.append("</ul><h2>");
+        builder.append("Spring Boot Actuator");
+        builder.append("</h2><ul>");
+        builder.append("<li><a href=\"/act/actuator/\">actuator</a></li>");
+        endpoints.forEach(endpoint ->
+            builder.append("<li><a href=\"/act/" + endpoint.getId()
+                + "\">" + endpoint.getId() + "</a></li>"));
         builder.append("</ul></body></html>");
         return builder.toString();
 	}
-
 }
