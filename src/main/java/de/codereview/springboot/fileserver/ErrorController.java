e76b227445691273a5cd0a94e429f2a33425ebb4
Frank Bruch <frank.bruch@shks.de>
2019-04-10 21:18:15 +0200

    WIP: Spring Boot 2

--- src/main/java/de/codereview/springboot/fileserver/ErrorController.java
+++ src/main/java/de/codereview/springboot/fileserver/ErrorController.java
@@ -1,12 +1,12 @@
 package de.codereview.springboot.fileserver;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.ErrorAttributes;
 import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.context.request.WebRequest;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
@@ -16,9 +16,8 @@ import java.util.Map;
  */
 @RestController
 @RequestMapping("/error")
-public class ErrorController implements org.springframework.boot.autoconfigure.web.ErrorController
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController
 {
-
 	private final ErrorAttributes errorAttributes;
 
 	@Autowired
@@ -34,8 +33,10 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
 		return "/error";
 	}
 
+
+
 	@RequestMapping
-	public Map<String, Object> error(HttpServletRequest aRequest)
+	public Map<String, Object> error(WebRequest aRequest)
 	{
 		Map<String, Object> body = getErrorAttributes(aRequest, getTraceParameter(aRequest));
 		String trace = (String) body.get("trace");
@@ -47,7 +48,7 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
 		return body;
 	}
 
-	private boolean getTraceParameter(HttpServletRequest request)
+	private boolean getTraceParameter(WebRequest request)
 	{
 		String parameter = request.getParameter("trace");
 		if (parameter == null)
@@ -57,9 +58,8 @@ public class ErrorController implements org.springframework.boot.autoconfigure.w
 		return !"false".equals(parameter.toLowerCase());
 	}
 
-	private Map<String, Object> getErrorAttributes(HttpServletRequest aRequest, boolean includeStackTrace)
+	private Map<String, Object> getErrorAttributes(WebRequest aRequest, boolean includeStackTrace)
 	{
-		RequestAttributes requestAttributes = new ServletRequestAttributes(aRequest);
-		return errorAttributes.getErrorAttributes(requestAttributes, includeStackTrace);
+		return errorAttributes.getErrorAttributes(aRequest, includeStackTrace);
 	}
 }
