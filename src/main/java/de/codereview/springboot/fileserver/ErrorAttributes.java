e1f9132a4bf72bb715e4afad7aabf2f6e943a8f1
Frank Bruch <frank.bruch@shks.de>
2019-04-10 21:18:15 +0200

    WIP: Spring Boot 2

--- src/main/java/de/codereview/springboot/fileserver/ErrorAttributes.java
+++ src/main/java/de/codereview/springboot/fileserver/ErrorAttributes.java
@@ -1,33 +1,32 @@
 package de.codereview.springboot.fileserver;
 
-import org.springframework.boot.autoconfigure.web.DefaultErrorAttributes;
-import org.springframework.stereotype.Component;
-import org.springframework.web.context.request.RequestAttributes;
-
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.WebRequest;
+
 /**
- * Thanks, https://blog.jdriven.com/2016/06/spicy-spring-custom-error-json-response-with-errorattributes/
+ * Thanks,
+ * https://blog.jdriven.com/2016/06/spicy-spring-custom-error-json-response-with-errorattributes/
  */
 @Component
 public class ErrorAttributes extends DefaultErrorAttributes {
 
-	@Override
-	public Map<String, Object> getErrorAttributes(RequestAttributes requestAttributes, boolean includeStackTrace) {
-		Map<String, Object> errorAttributes = super.getErrorAttributes(requestAttributes, includeStackTrace);
+  public Map<String, Object> getErrorAttributes(WebRequest webRequest, boolean includeStackTrace) {
+    Map<String, Object> errorAttributes = super.getErrorAttributes(webRequest, includeStackTrace);
 
-		Throwable throwable = getError(requestAttributes);
-		if (throwable != null)
-		{
-			Throwable cause = throwable.getCause();
-			if (cause != null) {
-				Map<String, Object> causeErrorAttributes = new HashMap<>();
-				causeErrorAttributes.put("exception", cause.getClass().getName());
-				causeErrorAttributes.put("message", cause.getMessage());
-				errorAttributes.put("cause", causeErrorAttributes);
-			}
-		}
-		return errorAttributes;
-	}
+    Throwable throwable = getError(webRequest);
+    if (throwable != null) {
+      Throwable cause = throwable.getCause();
+      if (cause != null) {
+        Map<String, Object> causeErrorAttributes = new HashMap<>();
+        causeErrorAttributes.put("exception", cause.getClass().getName());
+        causeErrorAttributes.put("message", cause.getMessage());
+        errorAttributes.put("cause", causeErrorAttributes);
+      }
+    }
+    return errorAttributes;
+  }
 }
